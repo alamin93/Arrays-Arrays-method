440a4c8cefcf5b1b956cf12403698cab4b9e42cf
S M AL AMIN <76505639+alamin93@users.noreply.github.com>
2020-12-26 16:11:50 +0600

    Arrays & Arrays method

---
+++ index.js
@@ -0,0 +1,30 @@
+// arrays & array methods
+var numbers =[20,40,50,60,70,200];
+var color = ["red","blue","green","yellow"]
+//console.log(numbers); print korar jonno
+// console.log(numbers.length); array number janar jonno
+// console.log(numbers.concat(color)); duita array add korar jonno
+
+let value;
+value = numbers.length;
+// value = Array.isArray(numbers); ata arrry na onno kisu seta janan jonno
+// value = numbers[2]; index number janar jonno
+//numbers[0] = 100; mutetin nonmutation korer jonno
+//value =numbers.indexOf(50) koto number index a 50 ase ta janar jonno
+//numbers.push(250); // array sesh a index add korar jonno push use kora history
+//numbers.unshift(260);// array first a index add korar jonno push use kora history
+
+//numbers.pop() // last index bad korar jono
+//numbers.shift()// first index bad korar jono
+//numbers.splice(1,2) // majer kono index bad diyer jonno ba majer koto gula index bad deyer jonno
+//numbers.reverse() // array index gula reverse hoye show hobe.
+//value = color.sort(); letter wise prothom theke saganor jonno
+//value = numbers.sort(); ata kon value oujayi srial hoy na number er prothom j digit thakbe seta dhore srial hobe coto theke boro 
+//value =numbers.sort(function(x,y){return x-y;}); //sriali soto theke boro sajanor jonno use hobe this code
+//value =numbers.sort(function(x,y){return y-x;}); //sriali boro theke soto sajanor jonno use hobe this code
+
+
+
+//outpur
+console.log(numbers);
+console.log(value);
